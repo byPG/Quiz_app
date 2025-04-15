51144186a659db66e40c9dc3b0b051ade5a85ec3
byPG <67753466+byPG@users.noreply.github.com>
2025-04-15 23:55:51 +0200

    initialization with some style

---
+++ main.js
@@ -0,0 +1,44 @@
+const quizData = [
+    {
+        question: 'How old is Paulina?',
+        a: '10',
+        b: '18',
+        c: '29',
+        d: '30',
+        correct: 'd'
+    },
+    {
+        question: 'Who is the President of the USA?',
+        a: 'Ivan Saldano',
+        b: 'Donald Trump',
+        c: 'Barack Obama',
+        d: 'Paulina :D',
+        correct: 'd'
+    },
+    {
+        question: 'What is the best programming language? :) ',
+        a: 'Java',
+        b: 'JavaScript',
+        c: 'Python',
+        d: 'PHP',
+        correct: 'b'
+    },
+    {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Application Programming Interface',
+        correct: 'a'
+    },
+
+    {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Application Programming Interface',
+        correct: 'a'
+    },
+
+]
