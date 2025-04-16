ad26b55af95821c7b4e2776c84eb18ab4fc06ae9
byPG <67753466+byPG@users.noreply.github.com>
2025-04-17 00:01:11 +0200

    optymalization

--- main.js
+++ main.js
@@ -55,34 +55,27 @@ const submitBtn = document.getElementById('submit');
 let currentQuiz = 0;
 let score= 0;
 
-
-loadQuiz();
-
 function loadQuiz(){
     deselectAnswers()
-
     const currentQuizData = quizData[currentQuiz];
     questionEl.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
-
-  
 }
+loadQuiz();
+
 
 function getSelected(){
  let answer = undefined;
-
     answersEls.forEach((answerEl)=>{
        if(answerEl.checked){
        answer = answerEl.id;
        }
     });
-
    return answer;
 }
-
 getSelected();
 
 function deselectAnswers(){
@@ -94,7 +87,6 @@ function deselectAnswers(){
 
 submitBtn.addEventListener('click', () => {
     const answer = getSelected();
-    console.log(answer)
 
     if(answer)
         if(answer === quizData[currentQuiz].correct){
